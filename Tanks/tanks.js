78de9ec688f95ae37b71b774204839ea2d8bb4e6
Alen-Nikolov <alennikolov95@gmail.com>
2017-04-04 00:50:06 +0300

    Made both tanks to be able to move

--- Tanks/tanks.js
+++ Tanks/tanks.js
@@ -10,96 +10,96 @@ $(function () {
     }, false);
     var movesHorizontally = 8;
     var movesVertically = 8;
-    console.log(tank2.position().top)
     var movesHorizontallyTank2 = tank2.position().left;
     var movesVerticallyTank2 = tank2.position().top;
+    var keys = {};
     function controlsForTank1() {
-        if (event.keyCode === 39) {
-            tank1.attr("class", "rotate90");
-            if (movesHorizontally <= window.innerWidth - 85) {
-                movesHorizontally += 8;
-            }
-            tank1.css("left", (movesHorizontally) + "px")
+        for (var i in keys) {
+            console.log(i);
+            if (i == 39) {
+                tank1.attr("class", "rotate90");
+                if (movesHorizontally <= window.innerWidth - 85) {
+                    movesHorizontally += 8;
+                }
+                tank1.css("left", (movesHorizontally) + "px")
 
-        }
-        //down
-        if (event.keyCode === 40) {
-            tank1.attr("class", "rotate180");
-            if (movesVertically <= 545) {
-                movesVertically += 8;
             }
-            tank1.css("top", (movesVertically) + "px")
+            //down
+            if (i == 40) {
+                tank1.attr("class", "rotate180");
+                if (movesVertically <= 545) {
+                    movesVertically += 8;
+                }
+                tank1.css("top", (movesVertically) + "px")
 
-        }
-        //left
-        if (event.keyCode === 37) {
-            tank1.attr("class", "rotate270");
-            if (movesHorizontally >= 18) {
-                movesHorizontally -= 8;
             }
-            tank1.css("left", (movesHorizontally) + "px")
+            //left
+            if (i == 37) {
+                tank1.attr("class", "rotate270");
+                if (movesHorizontally >= 18) {
+                    movesHorizontally -= 8;
+                }
+                tank1.css("left", (movesHorizontally) + "px")
 
-        }
-        //up
-        if (event.keyCode === 38) {
-            tank1.attr("class", "rotate0");
-            if (movesVertically >= 18) {
-                movesVertically -= 8;
             }
-            tank1.css("top", (movesVertically) + "px")
+            //up
+            if (i == 38) {
+                tank1.attr("class", "rotate0");
+                if (movesVertically >= 18) {
+                    movesVertically -= 8;
+                }
+                tank1.css("top", (movesVertically) + "px")
 
+            }
+            if (i == 68) {
+                tank2.attr("class", "rotate90");
+
+                if (movesHorizontallyTank2 <= window.innerWidth - 85) {
+                    movesHorizontallyTank2 += 8;
+                }
+                tank2.css("left", (movesHorizontallyTank2) + "px")
+
+            }
+            //down
+            if (i == 83) {
+                tank2.attr("class", "rotate180");
+
+                if (movesVerticallyTank2 <= 545) {
+                    movesVerticallyTank2 += 8;
+                }
+                tank2.css("top", (movesVerticallyTank2) + "px")
+
+            }
+            //left
+            if (i == 65) {
+                tank2.attr("class", "rotate270");
+
+                if (movesHorizontallyTank2 >= 18) {
+                    movesHorizontallyTank2 -= 8;
+                }
+                tank2.css("left", (movesHorizontallyTank2) + "px")
+
+            }
+            //up
+            if (i == 87) {
+                tank2.attr("class", "rotate0");
+
+                if (movesVerticallyTank2 >= 18) {
+                    movesVerticallyTank2 -= 8;
+                }
+                tank2.css("top", (movesVerticallyTank2) + "px")
+
+            }
         }
     }
-    function controlsForTank2() {
-        if (event.keyCode === 68) {
-            tank2.attr("class", "rotate90");
 
-            if (movesHorizontallyTank2 <= window.innerWidth - 85) {
-                movesHorizontallyTank2 += 8;
-            }
-            tank2.css("left", (movesHorizontallyTank2) + "px")
-
-        }
-        //down
-        if (event.keyCode === 83) {
-            tank2.attr("class", "rotate180");
-
-            if (movesVerticallyTank2 <= 545) {
-                movesVerticallyTank2 += 8;
-            }
-            tank2.css("top", (movesVerticallyTank2) + "px")
-
-        }
-        //left
-        if (event.keyCode === 65) {
-            tank2.attr("class", "rotate270");
-
-            if (movesHorizontallyTank2 >= 18) {
-                movesHorizontallyTank2 -= 8;
-            }
-            tank2.css("left", (movesHorizontallyTank2) + "px")
-
-        }
-        //up
-        if (event.keyCode === 87) {
-            tank2.attr("class", "rotate0");
-
-            if (movesVerticallyTank2 >= 18) {
-                movesVerticallyTank2 -= 8;
-            }
-            tank2.css("top", (movesVerticallyTank2) + "px")
-
-        }
-    }
     $(document).on('keydown', function (event) {
+        keys[event.which] = true;
         controlsForTank1();
-        controlsForTank2()
-
-        //Controls For tank2
 
     })
-
-
-
-
+    $(document).keyup(function (e) {
+        delete keys[e.which];
+        controlsForTank1();
+    });
 })
